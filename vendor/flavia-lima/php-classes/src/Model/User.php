633ad50b3256e6eafea4169f9376486dd26906c5
Flavia Lima <mbl.flavia@gmail.com>
2020-08-29 22:54:00 -0300

    Admin - recuperação de senha através de link enviado por e-mail funcionando.

--- vendor/flavia-lima/php-classes/src/Model/User.php
+++ vendor/flavia-lima/php-classes/src/Model/User.php
@@ -4,10 +4,13 @@ namespace Flavia\Model;
 
 use \Flavia\DB\Sql;
 use \Flavia\Model;
+use \Flavia\Mailer;
 
 class User extends Model{
 
 	const SESSION ="User";
+	const SECRET ="Flavia_GeekStore"; //Chave de criptografia.
+	const SECRET_IV = "Flavia_GeekStore_Secret_IV";
 
 	protected $fields = [
 		"id_user", "id_person", "des_login", "des_password", "email", "phone", "cpf", "inadmin", "des_person"
@@ -169,6 +172,126 @@ class User extends Model{
 		$sql = new Sql();
 
 		$sql->query("CALL sp_users_delete(:id_user)", array(
+			":id_user"=>$this->getid_user()
+		));
+
+	}
+
+	public static function getForgot($email)
+	{
+
+		$sql =new Sql();
+
+		$results = $sql->select("
+			SELECT * 
+			FROM tb_persons a
+			INNER JOIN tb_users b USING(id_person)
+			WHERE a.email = :email;
+		", array(
+			":email"=>$email
+		));
+
+		//Caso o usuário informe um e-mail que não existe.
+		if(count($results) === 0)
+		{
+
+			throw new \Exception("Não foi possível recuperar a senha.");
+			
+
+		} else {
+
+			$data = $results[0];
+
+			$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:id_user, :des_ip)", array(
+				":id_user"=>$data["id_user"],
+				":des_ip"=>$_SERVER["REMOTE_ADDR"] //Pega o ip do usuário.
+			));
+
+			if(count($results2) === 0 )
+			{
+
+				throw new \Exception("Não foi possível recuperar a senha.");
+
+			} else {
+
+				$dataRecovery = $results2[0];
+
+				$code = openssl_encrypt($dataRecovery['id_recovery'], 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+				$code = base64_encode($code);
+
+				$link = "http://www.projetophp.com.br/admin/forgot/reset?code=$code";
+
+				$mailer = new Mailer($data['email'], $data['des_person'], "Redefinir senha da Geek Store", "forgot", array(
+					"name"=>$data['des_person'],
+					"link"=>$link
+				));
+
+				$mailer->send();
+
+				return $data;
+
+			}
+
+		}
+
+	}
+
+	public static function validForgotDecrypt($code)
+	{
+
+		$code = base64_decode($code);
+
+		$id_recovery = openssl_decrypt($code, 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT *
+			FROM tb_users_passwords_recoveries a
+			INNER JOIN tb_users b USING(id_user)
+			INNER JOIN tb_persons c USING(id_person)
+			WHERE
+				a.id_recovery = :id_recovery
+				AND
+				a.dt_recovery IS NULL
+				AND
+				DATE_ADD(a.dt_register, INTERVAL 1 HOUR) >= NOW();
+		", array(
+			":id_recovery"=>$id_recovery
+		));
+
+		if(count($results) === 0)
+		{
+
+			throw new \Exception("Não foi possível recuperar a senha.");
+
+		} else {
+
+			return $results[0];
+
+		}
+
+	}
+
+	public static function setForgotUsed($id_recovery)
+	{
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_users_passwords_recoveries SET dt_recovery = NOW() WHERE id_recovery = :id_recovery", array(
+			":id_recovery"=>$id_recovery
+		));
+
+	}
+
+	public function setPassword($password)
+	{
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_users SET des_password = :password WHERE id_user = :id_user", array(
+			":password"=>$password,
 			":id_user"=>$this->getid_user()
 		));
 
